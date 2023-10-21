1227c419ba0f9f131adf711e238c9d901dab7a49
DKM24 <daud.k.marwat24@gmail.com>
2023-10-21 20:01:28 +0500

    First table

--- src/multTable.c
+++ src/multTable.c
@@ -23,5 +23,16 @@ int main(int argc, char **argv) {
 
   //TODO: place your code here
 
+  for(int i=1;i<=n;i++){
+
+    for(int j=1;j<=n;j++){
+
+      prinf("%d     ")
+    } 
+
+
+
+  }
+
   return 0;
 }
